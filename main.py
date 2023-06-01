331d354529eea86b7eee5ba05e143230b3464b9b
Eduardo Faustino <83729062+Faustinooo@users.noreply.github.com>
2023-06-01 15:42:29 -0300

    ESTILIZAÇÃO

--- main.py
+++ main.py
@@ -49,4 +49,16 @@ def gerador(a = 1):
     for i in lista:
         fim += i
     return fim
-print(gerador(1))
+def linha(a = 0):
+    print(f"\033[1;{a}m-=\033[m" * 20)
+def cabeçalho(a = "TEXTO"):
+    print(f"\033[1m{a.center(40)}\033[m")
+def texto(a = "TEXTO"):
+    linha(32)
+    cabeçalho(a)
+    linha(31)
+texto("GERADOR DE CPF")
+print()
+print(gerador(1).center(40))
+print()
+linha()
